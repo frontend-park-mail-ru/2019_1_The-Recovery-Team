004360a4c0fc07a6a107c8ea3b336a999a64fa49
VitalyCherkov <vv-ch@bk.ru>
2019-03-09 11:53:38 +0300

    fixed update tree

--- src/libs/CheburactDOM/updateTree/index.ts
+++ src/libs/CheburactDOM/updateTree/index.ts
@@ -1,109 +1,13 @@
 import { IComponent, IElement, IVirtualNode } from 'libs/Cheburact/types';
-import { IRootContext, getTreeBuilder, rootContext } from '../utils';
+import { getTreeBuilder, IRootContext } from '../utils';
 import { FiberTypes, IFiberNode, UpdateQueueItem } from '../types';
 
 import eraseEqualFiberCollectionItem from './eraseEqualFiberCollectionItem';
 import getFlatArrayFromCollection from './getFlatArrayFromCollection';
 import findItemInUpdateQueue from './findItemInUpdateQueue';
 import spreadFibersByType from './spreadFibersByType';
-import { FiberCollectionItem } from './types';
 import unmount from './unmount';
 
-const getTreesReconciler = (
-    updateQueue: Array<UpdateQueueItem>
-) => {
-
-  const reconcileTrees = (
-      $target: HTMLElement,
-      fibers: Array<IFiberNode>,
-      elements: Array<IElement>,
-  ): Array<IFiberNode> => {
-
-    const updateOrCreateFiberWithElement = (
-        $target: HTMLElement,
-        fiber: IFiberNode | null,
-        element: IElement,
-    ): IFiberNode | null => {
-      if (!fiber) {
-        if (!rootContext.updater) {
-          return null;
-        }
-
-        const builtTree = getTreeBuilder(rootContext)($target, element);
-        if (!builtTree || builtTree.length === 0) {
-          return null;
-        }
-        return builtTree[ 0 ];
-      }
-
-      if (!fiber.ref && fiber.type !== FiberTypes.COMPONENT) {
-        return null;
-      }
-
-      if (fiber.type === FiberTypes.STRING) {
-        return rootContext.updateTextFiber(fiber, element as string);
-      }
-
-      if (fiber.type === FiberTypes.VIRTUAL_NODE) {
-        const nextFiber = rootContext.updateVNodeFiber(fiber, element as IVirtualNode);
-        if (!nextFiber) {
-          return null;
-        }
-        nextFiber.children = reconcileTrees(
-            (nextFiber.ref as HTMLElement),
-            nextFiber.children,
-            (element as IVirtualNode).children
-        );
-        return nextFiber;
-      }
-
-      if (fiber.type === FiberTypes.COMPONENT) {
-        const prevComp = fiber.stateNode as IComponent;
-        const nextComp = element as IComponent;
-
-        const { nextState } = findItemInUpdateQueue(prevComp, updateQueue) || {};
-        prevComp.writeState({ ...prevComp.getState(), ...(nextState || {}) });
-        prevComp['props'] = nextComp.getProps();
-
-        let renderedTree: any = prevComp.render();
-        if (!Array.isArray(renderedTree)) {
-          renderedTree = [renderedTree];
-        }
-
-        fiber.children = reconcileTrees($target, fiber.children, renderedTree);
-        fiber.stateNode = prevComp;
-        return fiber;
-      }
-      return null;
-    };
-
-    const collection = spreadFibersByType(fibers);
-    const relatedItems: Array<[FiberCollectionItem | null, IElement]>
-      = elements.map((el: IElement) => ([eraseEqualFiberCollectionItem(collection, el), el]));
-
-    // Удаляем оставшиеся элементы
-    const itemsToDelete = getFlatArrayFromCollection(collection);
-    itemsToDelete.forEach((item) => item && unmount(item.fiber));
-
-    // Обновляем и вставляем в target с текущего индекса
-    // TODO:
-    const nextChildren: Array<IFiberNode> = [];
-    relatedItems.forEach(([item, el]) => {
-      const fiber = item ? (item as FiberCollectionItem).fiber : null;
-      const nextFiber = updateOrCreateFiberWithElement($target, fiber, el);
-      if (nextFiber) {
-        nextChildren.push(nextFiber);
-        if (nextFiber.type !== FiberTypes.COMPONENT && nextFiber.ref) {
-          $target.appendChild(nextFiber.ref);
-        }
-      }
-    });
-
-    return nextChildren;
-  };
-
-  return reconcileTrees;
-};
 
 export default function updateTree(
     rootContext: IRootContext,
@@ -116,6 +20,94 @@ export default function updateTree(
     return null;
   }
 
+  const updateOrCreateFiberWithElement = (
+      $target: HTMLElement,
+      fiber: IFiberNode | null,
+      element: IElement,
+  ): IFiberNode | null => {
+    if (!rootContext.updater || fiber && !fiber.ref && fiber.type !== FiberTypes.COMPONENT) {
+      return null;
+    }
+
+    if (!fiber) {
+      const builtTree = getTreeBuilder(rootContext)($target, element);
+      if (!builtTree || builtTree.length === 0) {
+        return null;
+      }
+      return builtTree[ 0 ];
+    }
+
+    if (fiber.type === FiberTypes.STRING) {
+      return rootContext.updateTextFiber(fiber, element as string);
+    }
+
+    if (fiber.type === FiberTypes.VIRTUAL_NODE) {
+      const nextFiber = rootContext.updateVNodeFiber(fiber, element as IVirtualNode);
+      if (!nextFiber) {
+        return null;
+      }
+      nextFiber.children = reconcileTrees(
+          (nextFiber.ref as HTMLElement),
+          nextFiber.children,
+          (element as IVirtualNode).children
+      );
+      return nextFiber;
+    }
+
+    if (fiber.type === FiberTypes.COMPONENT) {
+      const prevComp = fiber.stateNode as IComponent;
+      const nextComp = element as IComponent;
+
+      const { nextState } = findItemInUpdateQueue(prevComp, updateQueue) || {};
+
+      prevComp.writeState({ ...prevComp.getState(), ...(nextState || {}) });
+      const prevProps = prevComp.getProps();
+      prevComp['props'] = nextComp.getProps();
+      prevComp.componentDidUpdate(prevProps);
+
+      let renderedTree: any = prevComp.render();
+      if (!Array.isArray(renderedTree)) {
+        renderedTree = [renderedTree];
+      }
+
+      fiber.children = reconcileTrees($target, fiber.children, renderedTree);
+      fiber.stateNode = prevComp;
+      return fiber;
+    }
+    return null;
+  };
+
+  const reconcileTrees = (
+      $target: HTMLElement,
+      fibers: Array<IFiberNode>,
+      elements: Array<IElement>,
+  ): Array<IFiberNode> => {
+    const collection = spreadFibersByType(fibers);
+    const itemsToUpdate = elements.map((el) => ([
+      eraseEqualFiberCollectionItem(collection, el),
+      el,
+    ]));
+
+    // Удаляем оставшиеся элементы
+    const itemsToDelete = getFlatArrayFromCollection(collection);
+    itemsToDelete.forEach((item) => item && unmount(item.fiber));
+
+    // Обновляем и вставляем в target с текущего индекса
+    const nextChildren: Array<IFiberNode> = [];
+    itemsToUpdate.forEach(([item, el]) => {
+      const { fiber } = (item || { fiber: null });
+      const nextFiber = updateOrCreateFiberWithElement($target, fiber, el as IElement);
+      if (nextFiber) {
+        nextChildren.push(nextFiber);
+        if (!fiber && nextFiber.type !== FiberTypes.COMPONENT && nextFiber.ref) {
+          $target.appendChild(nextFiber.ref);
+        }
+      }
+    });
+
+    return nextChildren;
+  };
+
   const bypassFiber = (fiberNodes: Array<IFiberNode>, $target: HTMLElement): Array<IFiberNode> => {
     return fiberNodes.map((node: IFiberNode) => {
       const $nextTarget = node.ref instanceof HTMLElement ? node.ref : $target;
@@ -123,14 +115,15 @@ export default function updateTree(
       if (node.type === FiberTypes.COMPONENT) {
         const qItem = findItemInUpdateQueue(node.stateNode as any, updateQueue);
         if (qItem) {
-          const element: IComponent = qItem.fiberNode.stateNode as IComponent;
+          const element = qItem.fiberNode.stateNode as IComponent;
           element.writeState(qItem.nextState);
+          element.componentDidUpdate(element.getProps());
           let renderedTree: any = element.render();
           if (!Array.isArray(renderedTree)) {
             renderedTree = [renderedTree];
           }
 
-          node.children = getTreesReconciler(updateQueue)($target, node.children, renderedTree);
+          node.children = reconcileTrees($target, node.children, renderedTree);
           return node;
         }
       }
