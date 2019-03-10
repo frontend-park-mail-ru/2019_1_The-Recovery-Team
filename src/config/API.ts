b28ecaacc1c77ef781534d37acb8b863dd85d9e5
VitalyCherkov <vv-ch@bk.ru>
2019-03-10 22:30:04 +0300

    changed back url

--- src/config/API.ts
+++ src/config/API.ts
@@ -1,4 +1,7 @@
-export const BASE_URL = '';
+export const BASE_URL =
+    window.location.hostname === '127.0.0.1'
+    || window.location.hostname === 'localhost'
+        ? '' : 'http://104.248.28.45:8080';
 
 const prefixAPI = (url) => `${BASE_URL}${url}`;
 
