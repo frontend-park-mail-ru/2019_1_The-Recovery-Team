23b7b638e64891feb707fd4977c03328353b0c3c
VitalyCherkov <vv-ch@bk.ru>
2019-04-07 22:03:32 +0300

    added link to game

--- src/App/SitePart/StartPage/StartPage.tsx
+++ src/App/SitePart/StartPage/StartPage.tsx
@@ -1,5 +1,6 @@
 import PlayButton, { PlayButtonModes } from 'components/buttons/PlayButton';
 import Logotype, { LogotypeSizes } from 'components/Logotype';
+import { routeCreators } from 'config/routes';
 import * as React from 'libs/Cheburact';
 import classNames from 'libs/classNames';
 const styles = require('./StartPage.modules.scss');
@@ -7,27 +8,15 @@ const styles = require('./StartPage.modules.scss');
 const cn = classNames(styles);
 
 export default class StartPage extends React.Component {
-  logoRef = null;
-  hidden = false;
-
-  componentDidMount() {
-    setInterval(() => {
-      this.logoRef.hidden = this.hidden;
-      this.hidden = !this.hidden;
-    }, 1000);
-  }
-
   render() {
     return (
       <div className={cn('start-page')}>
         <Logotype size={LogotypeSizes.LARGE} />
-        <div
-          className={cn('start-page__play-buttons')}
-          ref={r => (this.logoRef = r)}
-        >
+        <div className={cn('start-page__play-buttons')}>
           <PlayButton
             className={cn('start-page__play-button')}
             mode={PlayButtonModes.SINGLEPLAYER}
+            to={routeCreators.TO_GAME_PART()}
           />
           <PlayButton
             className={cn('start-page__play-button')}
