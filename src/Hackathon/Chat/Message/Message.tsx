1e8a519a2c7c23b6d94fd7ad1d0d1b170d2f27c7
FedorBilichenko <bilichenko.fedor@yandex.ru>
2019-04-27 13:47:02 +0300

    испралвена анонимный юзер

--- src/Hackathon/Chat/Message/Message.tsx
+++ src/Hackathon/Chat/Message/Message.tsx
@@ -2,12 +2,18 @@ import AvatarProfile from 'components/AvatarProfile';
 import * as React from 'libs/Cheburact';
 import classNames from 'libs/classNames';
 const styles = require('./Message.modules.scss');
+const anonAvatar = require('config/img/anonymousAvatar.jpg');
 
 const cn = classNames(styles);
 
 export default class Message extends React.Component {
   render() {
-    const { user, text, isMine = false } = this.props;
+    const anonimUser = {
+      nickname: 'Аноним',
+      anonAvatar,
+    }
+    const { user = anonimUser, text, isMine = false } = this.props;
+   
 
     return (
       <div className={cn('message', isMine && 'message_mine')}>
@@ -15,7 +21,7 @@ export default class Message extends React.Component {
           <AvatarProfile user={user} className={cn('message__avatar')} />
         ) : null}
         <div className={cn('message__text', isMine && 'message__text_mine')}>
-          {!isMine && (<div className={cn('message__nickname')}>{user ? user.nickname : 'Аноним'}</div>)}
+          {!isMine && (<div className={cn('message__nickname')}>{user.nickname}</div>)}
           {text}
         </div>
       </div>
