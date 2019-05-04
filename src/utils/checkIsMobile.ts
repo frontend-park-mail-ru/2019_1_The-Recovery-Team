5f9f73e194a47eceb6320a1d00fffe2fade7eeed
FedorBilichenko <bilichenko.fedor@yandex.ru>
2019-05-04 13:09:32 +0300

    на resise подвешена функция isMobile, убран footer в моб версии, исправлены кнопки

--- src/utils/checkIsMobile.ts
+++ src/utils/checkIsMobile.ts
@@ -1,8 +1,18 @@
 const MOBILE_SIDE = 550;
 
-export default () => {
+export const checkIsMobile = () => {
   const { clientHeight, clientWidth } = window.document.body;
   if (Math.min(clientHeight, clientWidth) <= MOBILE_SIDE) {
     document.body.classList.add('isMobile');
+
+    if (clientHeight < clientWidth) {
+      document.body.classList.add('isHorizontalMobile');
+    } else {
+      document.body.classList.remove('isHorizontalMobile');
+    }
+  } else {
+    document.body.classList.remove('isMobile', 'isHorizontalMobile');
   }
 };
+
+window.addEventListener('resize', checkIsMobile);
