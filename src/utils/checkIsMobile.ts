6b2842265f308cd3e70b0aeaa6da5d97482788b9
FedorBilichenko <bilichenko.fedor@yandex.ru>
2019-05-04 13:28:24 +0300

    исправдена функция checkIsMobile

--- src/utils/checkIsMobile.ts
+++ src/utils/checkIsMobile.ts
@@ -14,6 +14,7 @@ const checkIsMobile = () => {
       document.body.classList.remove('isHorizontalMobile');
     }
   } else {
+    isMobile = false;
     document.body.classList.remove('isMobile', 'isHorizontalMobile');
   }
 };
